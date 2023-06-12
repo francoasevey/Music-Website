939364245b5d289ab813f96a4f287be9bcaeb96b
francoromeo <francoromeo5012@gmail.com>
2023-06-12 13:25:11 -0300

    all music objet type

--- script.js
+++ script.js
@@ -13,6 +13,72 @@ const songs = [
         <div class="subtitle">Alan Walker</div>`,
         poster: "img/2.jpg"
     },
+    // all object type 
+    {
+        id:"3",
+        songName: `Cartoon - On & On <br><div class="subtitle"> Daniel Levi</div>`,
+        poster: "img/3.jpg",
+    },
+    {
+        id:"4",
+        songName: `Warriyo - Mortals <br><div class="subtitle">Mortals</div>`,
+        poster: "img/4.jpg",
+    },
+    {
+        id:"5",
+        songName: `Ertugrul Gazi <br><div class="subtitle">Ertugrul</div>`,
+        poster: "img/5.jpg",
+    },
+    {
+        id:"6",
+        songName: `Electronic Music <br><div class="subtitle">Electro</div>`,
+        poster: "img/6.jpg",
+    },
+    {
+        id:"7",
+        songName: `Agar Tum Sath Ho <br><div class="subtitle">Tamashaa</div>`,
+        poster: "img/7.jpg",
+    },
+    {
+        id:"8",
+        songName: `Suna Hai <br><div class="subtitle">Neha Kakker</div>`,
+        poster: "img/8.jpg",
+    },
+    {
+        id:"9",
+        songName: `Dilber <br><div class="subtitle">Satyameva Jayate</div>`,
+        poster: "img/9.jpg",
+    },
+    {
+        id:"10",
+        songName: `Duniya <br><div class="subtitle">Luka Chuppi</div>`,
+        poster: "img/10.jpg",
+    },
+    {
+        id:"11",
+        songName: `Lagdi Lahore Di <br><div class="subtitle">Street Dancer 3D</div>`,
+        poster: "img/11.jpg",
+    },
+    {
+        id:"12",
+        songName: `Putt Jatt Da <br><div class="subtitle">Putt Jatt Da</div>`,
+        poster: "img/12.jpg",
+    },
+    {
+        id:"13",
+        songName: `Baarishein <br><div class="subtitle">Atif Aslam</div>`,
+        poster: "img/13.jpg",
+    },
+    {
+        id:"14",
+        songName: `Vaaste <br><div class="subtitle">Dhvani Bhanushali</div>`,
+        poster: "img/14.jpg",
+    },
+    {
+        id:"15",
+        songName: `Lut Gaye <br><div class="subtitle">Jubin Nautiyal</div>`,
+        poster: "img/15.jpg",
+    },
 ]
 Array.from(document.getElementsByClassName('songItem')).forEach((element, i)=>{
     element.getElementsByTagName('img')[0].src = songs[i].poster;
